bac852601728633dae651fdc291d7ba2844158d5
melon <ortizfranco48@gmail.com>
2023-04-11 16:16:17 -0300

    PaymentController

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
+const PaymentController = require("../Controllers/PaymentsController");
+const PaymentsService = require("../Services/PaymentsService");
+const PaymentInstance = new PaymentController(new PaymentsService());
+
+// routes -------------------------------------------------
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/payment", function (req, res, next) {
+  PaymentInstance.createPayment(req, res);
+});
+
+router.get("/subscription", function (req, res, next) {
+  PaymentInstance.createSubscription(req, res);
 });
 
 module.exports = router;
